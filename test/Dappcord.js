20bc4ad4312f344b0ecc860b067f726d6583dbd6
Tusharbecoding <tusharchopra02122002@gmail.com>
2024-01-19 14:56:36 +0530

    changes to dappcord .js and .sol

--- test/Dappcord.js
+++ test/Dappcord.js
@@ -1,9 +1,33 @@
-const { expect } = require("chai")
+const { expect } = require("chai");
 
 const tokens = (n) => {
-  return ethers.utils.parseUnits(n.toString(), 'ether')
-}
+  return ethers.utils.parseUnits(n.toString(), "ether");
+};
 
 describe("Dappcord", function () {
+  let deployer, user;
+  let dappcord;
+  beforeEach(async () => {
+    [deployer, user] = await ethers.getSigners();
 
-})
+    const Dappcord = await ethers.getContractFactory("Dappcord");
+    dappcord = await Dappcord.deploy("Dappcord", "DC");
+  });
+
+  describe("Deplopyment", function () {
+    it("Sets the name", async () => {
+      let result = await dappcord.name();
+      expect(result).to.equal("Dappcord");
+    });
+
+    it("Sets the symbol", async () => {
+      let result = await dappcord.symbol();
+      expect(result).to.equal("DC");
+    });
+
+    it("Sets the owner", async () => {
+      let result = await dappcord.owner();
+      expect(result).to.equal(deployer.address);
+    });
+  });
+});
